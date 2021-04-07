63834c2a0b55fb9602f2d2de12e286bf10edfbee
cassandra jacques <cassandrajacques@cassandsMacBook.lan>
2021-04-06 23:00:01 -0700

    started header

--- PHPCurl/index.php
+++ PHPCurl/index.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 <body>
+
+<div class=nav>
+
 <?php
 $website = "";
 
@@ -37,10 +41,27 @@ function formAction($sitename) {
     echo $response;
 }
 
-$form = '<div class="urlform"><form method="get" target="_blank"> <p class="smallgrey">To what URL should this link go?</p><br><div class="url"><p>URL</p> <input class="textbox" placeholder="https://www.site.com" type="text" name="website"></div><br><br><input  class="submitButton" type="submit" name="submit" value="Submit"></form></div>';
-?>
+$form = '
+<div class="header">
+    <i class="far fa-times-circle"></i>
+    <h1 class="title">Simple Mock</h1>
+    <p class="subheader">Force ad creatives on any website</p>
+</div>
 
-<?php
+
+<div class="urlForm">
+    <form method="get" target="_blank"> 
+    <div class="urlBox">
+        <p>URL</p>
+    </div>
+    <div class="nonUrl">
+        <p class="smallgrey">To what URL should this link go?</p>
+        <br>
+        <input class="textbox" placeholder="https://www.site.com" type="text" name="website">
+    </div>
+    <input  class="submitButton" type="submit" name="submit" value="Submit"></form>
+</div>';
+
 if ($website) {
     formAction($website);
 } else {
