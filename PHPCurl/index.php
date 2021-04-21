0bd8bc11c2abb69974ff95ebf0cda9dec9633d03
cassandra jacques <cassandrajacques@cassandsMacBook.lan>
2021-04-20 20:47:32 -0700

    upload complete

--- PHPCurl/index.php
+++ PHPCurl/index.php
@@ -43,6 +43,7 @@
     }
 
     $form = '
+   
     <nav>
     <div class="header">
         <h2 class="title">Simple Mock</h2>
@@ -66,40 +67,29 @@
 </div>
 
 <div class="assets">
+<form id="assetsForm" class="boxForm" method="POST" action="upload.php" enctype="multipart/form-data">
 
     <h1 class="assetsTitle">Assets</h1>
     <p class="assetsText">Selected items will be featured on website</p>
 
-
     <div class="boxes">
-
         <div class="box">Horizontal
-            <form class="boxForm" method="POST" action="upload.php" enctype="multipart/form-data">
-                <input class="chooseFile" type="file" id="horizontal-upload" name="horizontalUpload">
-                <input class="file-upload" type="submit" name="horizontaluploadBtn" value="Upload" />
-            </form>
+            <input type="file" id="horizontal-upload" name="horizontalUpload">
+            <label for="horizontal-upload">Upload file</label>
         </div>
-
+        
         <div class="box">Vertical
-            <form class="boxForm" method="POST" action="upload.php" enctype="multipart/form-data">
-                <input class="chooseFile" type="file" id="vertical-upload" name="verticalUpload">
-                <input class="file-upload" type="submit" name="verticaluploadBtn" value="Upload" />
-            </form>
+            <input type="file" id="vertical-upload" name="verticalUpload" >
+            <label for="vertical-upload">Upload file</label>
         </div>
-
 
         <div class="box">Mobile
-            <form class="boxForm" method="POST" action="upload.php" enctype="multipart/form-data">
-                <input class="chooseFile" type="file" id="mobile-upload" name="mobileUpload">
-                <input class="file-upload" type="submit" name="mobileuploadBtn" value="Upload" />
-            </form>
+            <input type="file" id="mobile-upload" name="mobileUpload" >
+            <label for="mobile-upload">Upload file</label>
         </div>
-
     </div>
 
-
     <div class="urlForm">
-        <form method="get" target="_blank">
             <div class="urlBox">
                 <p>URL</p>
             </div>
@@ -108,9 +98,47 @@
                 <br>
                 <input class="textbox" placeholder="https://www.site.com" type="text" name="website">
             </div>
-            <input class="submitButton" type="submit" name="submit" value="Submit">
-        </form>
-    </div>';
+
+            <button class="submitButton" >Submit</button>
+            </form>
+            </div>
+            <script>
+            var myForm = document.getElementById("assetsForm");  
+            var myFiles = [
+                document.getElementById("horizontal-upload"),
+                document.getElementById("vertical-upload"),
+                document.getElementById("mobile-upload")
+            ];
+        
+            myForm.onsubmit = function(event)    { 
+                event.preventDefault();
+                var formData = new FormData();
+
+                function attachFile(f) {
+                    if (!f.files[0] || !f.files[0].type.match("image.*")) {
+                        return;
+                    }     
+                    formData.append(f.id, f.files[0], f.files[0].name);
+                }
+                myFiles.forEach(file => attachFile(file));
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "upload.php", true);
+        
+                xhr.onload = function () {
+                    if (xhr.status == 200) {
+                        let myURL = document.querySelector("input[name=website]").value;
+                        if (myURL.length || myURL !== "https://www.site.com") {
+                            window.open("index.php?website="+myURL, "_blank");
+                        } else {
+                            alert("Invalid URL");
+                        }
+                    }
+                };
+    
+                xhr.send(formData);
+            }
+        </script>';
 
 
     $extraJS = '
@@ -127,74 +155,82 @@
 
             var orientations = [
                 {
-                    name: "horizontal",
+                    name: "vertical",
                     sizes: [
-                        "970x250",
-                        "728x90",
-                        "970x90"
+                        "300, 250",
+                        "300, 600",
+                        "160, 600"
                     ]
                 },
                 {
-                    name: "vertical",
+                    name: "horizontal",
                     sizes: [
-                        "300x250",
-                        "300x600",
-                        "160x600"
+                        "970, 250",
+                        "728, 90",
+                        "970, 90"
                     ]
-                },
+                },                
                 {
                     name: "mobile",
                     sizes: [
-                        "300x50",
-                        "320x50"
+                        "300, 50",
+                        "320, 50"
                     ]
                 }
 
             ];
 
             function replaceIframe(iframe) {
-                if (iframe.width < 1){
+                if (!iframe || iframe.width < 1){
                     console.log("SimpleMock -- iFrame has no size.");
                     iframe.parentNode.removeChild(iframe);
                     return;
                 }
                 
-                var details = {
-                    name: "",
-                    width: iframe.width,
-                    height: iframe.height
-                };
+                var name = [];
 
-                var testSize = iframe.width + "x" + iframe.height;
+                var testSize = iframe.parentNode.parentNode.getAttribute("data-sizes");
 
-                function testOrientations(orientation) {
-                    console.log("testSize: " + testSize);
-                    if(orientation.sizes.includes(testSize)) {
-                        details.name = orientation.name;
-                    }
+                function testOrientations(ori) {
+                    ori.sizes.forEach(function(s){
+                        if(testSize.includes(s)){
+                            name.push(ori.name);
+                        }
+                    });
                 }
 
                 orientations.forEach(o => testOrientations(o));
 
-                if (details.name.length < 1){
+                if (name.length < 1){
                     console.log("SimpleMock -- Invalid iFrame Size. Rejecting.");
                     return;
                 }
 
-                var placeholder = document.createElement("img");
-                placeholder.src = "uploaded_files/" + details.name + ".jpeg";
-                iframe.parentNode.appendChild(placeholder);
-                iframe.parentNode.removeChild(iframe);
-                if (googletag) {
-                    googletag.destorySlots(iframe.parent.id);
+                if (name.length > 1) {
+                    if (window.innerWidth > 728) {
+                        name[0] = "horizontal";
+                    } else {
+                        name[0] = "mobile";
+                    }
                 }
 
-                // TO DO: add "natural size" of image to placeholder
-                function placeholderSize(){
-                    var placeholder = document.querySelector("#details");
-                    var realWidth = placeholder.naturalWidth;
-                    var realHeight = placeholder.naturalHeight;
+                var placeholder = document.createElement("img");
+                placeholder.src = "uploaded_files/" + name[0] + ".jpg";
+                placeholder.classList.add("simpleMock--ad");
+
+                iframe.parentNode.appendChild(placeholder);
+                iframe.parentNode.removeChild(iframe);
+
+                if (googletag) {
+                    googletag.cmd.push(function(){
+                        googletag.destorySlots();
+                    });
                 }
+                
+                placeholder.onload = function() {
+                    console.log("hey");
+                    this.setAttribute("style", "width: " + this.naturalWidth + "px !important;height: " + this.naturalHeight + "px !important;" );
+                };
             }
 
 
--- PHPCurl/upload.php
+++ PHPCurl/upload.php
@@ -1,134 +1,55 @@
 <?php
 session_start();
+$uploadFileDir = './uploaded_files/';
+$allowedfileExtensions = array('jpg', 'jpeg', 'gif', 'png');
 
-$message = ''; 
-if (isset($_POST['horizontaluploadBtn']) && $_POST['horizontaluploadBtn'] == 'Upload')
-{
-  if (isset($_FILES['horizontalUpload']) && $_FILES['horizontalUpload']['error'] === UPLOAD_ERR_OK)
-  {
-    $fileTmpPath = $_FILES['horizontalUpload']['tmp_name'];
-    $fileName = $_FILES['horizontalUpload']['name'];
-    $fileSize = $_FILES['horizontalUpload']['size'];
-    $fileType = $_FILES['horizontalUpload']['type'];
-    $fileNameCmps = explode(".", $fileName);
-    $fileExtension = strtolower(end($fileNameCmps));
+if (isset($_FILES['horizontal-upload']) && $_FILES['horizontal-upload']['error'] === UPLOAD_ERR_OK) {
+  $fileTmpPath1 = $_FILES['horizontal-upload']['tmp_name'];
+  $fileName1 = $_FILES['horizontal-upload']['name'];
+  $fileSize1 = $_FILES['horizontal-upload']['size'];
+  $fileType1 = $_FILES['horizontal-upload']['type'];
+  $fileNameCmps1 = explode(".", $fileName1);
+  $fileExtension1 = strtolower(end($fileNameCmps1));
+  $newFileName1 = 'horizontal' . '.' . $fileExtension1;
 
-    $newFileName = 'horizontal' . '.' . $fileExtension;
-
-    $allowedfileExtensions = array('jpg', 'jpeg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
-
-    if (in_array($fileExtension, $allowedfileExtensions))
-    {
-      $uploadFileDir = './uploaded_files/';
-      $dest_path = $uploadFileDir . $newFileName;
-
-      if(move_uploaded_file($fileTmpPath, $dest_path)) 
-      {
-        $message ='File is successfully uploaded.';
-      }
-      else 
-      {
-        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
-      }
+  if (in_array($fileExtension1, $allowedfileExtensions)){
+    $dest_path1 = $uploadFileDir . $newFileName1;
+    if(move_uploaded_file($fileTmpPath1, $dest_path1)) {
+      $message ='File 1 is successfully uploaded.';
     }
-    else
-    {
-      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
-    }
-  }
-  else
-  {
-    $message = 'There is some error in the file upload. Please check the following error.<br>';
-    $message .= 'Error:' . $_FILES['horizontalUpload']['error'];
   }
 }
-$_SESSION['message'] = $message;
-header("Location: index.php");
 
-$message = ''; 
-if (isset($_POST['verticaluploadBtn']) && $_POST['verticaluploadBtn'] == 'Upload')
-{
-  if (isset($_FILES['verticalUpload']) && $_FILES['verticalUpload']['error'] === UPLOAD_ERR_OK)
-  {
-    $fileTmpPath = $_FILES['verticalUpload']['tmp_name'];
-    $fileName = $_FILES['verticalUpload']['name'];
-    $fileSize = $_FILES['verticalUpload']['size'];
-    $fileType = $_FILES['verticalUpload']['type'];
-    $fileNameCmps = explode(".", $fileName);
-    $fileExtension = strtolower(end($fileNameCmps));
+if (isset($_FILES['vertical-upload']) && $_FILES['vertical-upload']['error'] === UPLOAD_ERR_OK) {
+  $fileTmpPath2 = $_FILES['vertical-upload']['tmp_name'];
+  $fileName2 = $_FILES['vertical-upload']['name'];
+  $fileSize2 = $_FILES['vertical-upload']['size'];
+  $fileType2 = $_FILES['vertical-upload']['type'];
+  $fileNameCmps2 = explode(".", $fileName2);
+  $fileExtension2 = strtolower(end($fileNameCmps2));
+  $newFileName2 = 'vertical' . '.' . $fileExtension2;
 
-    $newFileName = 'vertical' . '.' . $fileExtension;
-
-    $allowedfileExtensions = array('jpg', 'jpeg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
-
-    if (in_array($fileExtension, $allowedfileExtensions))
-    {
-      $uploadFileDir = './uploaded_files/';
-      $dest_path = $uploadFileDir . $newFileName;
-
-      if(move_uploaded_file($fileTmpPath, $dest_path)) 
-      {
-        $message ='File is successfully uploaded.';
-      }
-      else 
-      {
-        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
-      }
+  if (in_array($fileExtension2, $allowedfileExtensions)){
+    $dest_path2 = $uploadFileDir . $newFileName2;
+    if(move_uploaded_file($fileTmpPath2, $dest_path2)) {
+      $message ='File 2 is successfully uploaded.';
     }
-    else
-    {
-      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
-    }
-  }
-  else
-  {
-    $message = 'There is some error in the file upload. Please check the following error.<br>';
-    $message .= 'Error:' . $_FILES['verticalUpload']['error'];
   }
 }
-$_SESSION['message'] = $message;
-header("Location: index.php");
 
-$message = ''; 
-if (isset($_POST['mobileuploadBtn']) && $_POST['mobileuploadBtn'] == 'Upload')
-{
-  if (isset($_FILES['mobileUpload']) && $_FILES['mobileUpload']['error'] === UPLOAD_ERR_OK)
-  {
-    $fileTmpPath = $_FILES['mobileUpload']['tmp_name'];
-    $fileName = $_FILES['mobileUpload']['name'];
-    $fileSize = $_FILES['mobileUpload']['size'];
-    $fileType = $_FILES['mobileUpload']['type'];
-    $fileNameCmps = explode(".", $fileName);
-    $fileExtension = strtolower(end($fileNameCmps));
+if (isset($_FILES['mobile-upload']) && $_FILES['mobile-upload']['error'] === UPLOAD_ERR_OK) {
+  $fileTmpPath3 = $_FILES['mobile-upload']['tmp_name'];
+  $fileName3 = $_FILES['mobile-upload']['name'];
+  $fileSize3 = $_FILES['mobile-upload']['size'];
+  $fileType3 = $_FILES['mobile-upload']['type'];
+  $fileNameCmps3 = explode(".", $fileName3);
+  $fileExtension3 = strtolower(end($fileNameCmps3));
+  $newFileName3 = 'mobile' . '.' . $fileExtension3;
 
-    $newFileName = 'mobile' . '.' . $fileExtension;
-
-    $allowedfileExtensions = array('jpg', 'jpeg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
-
-    if (in_array($fileExtension, $allowedfileExtensions))
-    {
-      $uploadFileDir = './uploaded_files/';
-      $dest_path = $uploadFileDir . $newFileName;
-
-      if(move_uploaded_file($fileTmpPath, $dest_path)) 
-      {
-        $message ='File is successfully uploaded.';
-      }
-      else 
-      {
-        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
-      }
+  if (in_array($fileExtension3, $allowedfileExtensions)){
+    $dest_path3 = $uploadFileDir . $newFileName3;
+    if(move_uploaded_file($fileTmpPath3, $dest_path3)) {
+      $message ='File 3 is successfully uploaded.';
     }
-    else
-    {
-      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
-    }
-  }
-  else
-  {
-    $message = 'There is some error in the file upload. Please check the following error.<br>';
-    $message .= 'Error:' . $_FILES['mobileUpload']['error'];
   }
 }
-$_SESSION['message'] = $message;
-header("Location: index.php");
